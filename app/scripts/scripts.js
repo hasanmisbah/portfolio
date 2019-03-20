2c11a8a2786a23d2431b6f88f74605afb76a5e0b
Hasan Misbah <hasanmisbah01@gmail.com>
2019-03-21 00:26:29 +0600

    Completed Basic Things

--- app/scripts/scripts.js
+++ app/scripts/scripts.js
@@ -4,29 +4,28 @@ require('jquery-knob');
 require('slick-carousel');
 
 
-(function(){
-    $(window).on('load', function(){
-    // code to execute 
-    __has_header();
+(function () {
+    $(window).on('load', function () {
+        // code to execute 
+        __has_header();
         onLoad();
-    
+
     });
 
-    $(document).ready(function(){
-        $('#loader-wrapper').fadeOut(500);
-       
+    $(document).ready(function () {
+        preloader('#loader-wrapper', 4000);
         __has();
     });
 })();
 
-let __has = function(){
+let __has = function (data) {
     let __has = {
-        init : function(){
+        init: function () {
             this.stickynav('header', 'navbar_fixed');
             this.typed(['WordPress Developer', 'Linux Freak', 'FrontEnd Developer', 'WordPress Fanatic']);
             this.knob('.skill', '#013243');
             this.knob('.skill-f', '#1abc9c');
-            this.slick();
+            this.slick('.testimonial-items');
         },
         // setting navbar sticky
         stickynav: function (elem, class_name) {
@@ -39,7 +38,8 @@ let __has = function(){
                 }
             });
         },
-        typed : function(string){
+        // Typed Js
+        typed: function (string) {
             new Typed('.typed', {
                 strings: string,
                 typeSpeed: 40,
@@ -50,20 +50,22 @@ let __has = function(){
                 cursorChar: ' &Colone;',
             });
         },
-        knob : function(class_name, color){
+        // Knob Js 
+        knob: function (class_name, color) {
             $(class_name).knob({
-                width : '95%',
+                width: '95%',
                 skin: "tron",
-                fgColor : color,
-                readOnly : true,
-                thickness : '.4',
-                lineCap : 'round',
-                displayInput : true,
-                inputColor : color
+                fgColor: color,
+                readOnly: true,
+                thickness: '.4',
+                lineCap: 'round',
+                displayInput: true,
+                inputColor: color
             });
         },
-        slick : function(){
-            $(".testimonial-items").slick({
+        // Slick Slider
+        slick: function (class_name) {
+            $(class_name).slick({
                 infinite: true,
                 slidesToShow: 1,
                 slidesToScroll: 1,
@@ -73,16 +75,26 @@ let __has = function(){
         }
 
     };
-    __has.init();
+    // Call The Function
+    data = __has.init();
+    return data();
 };
 
 
-let __has_header = function(){
-  return window.LogRocket && window.LogRocket.init('f0lnrk/my-portfolio');
+
+// Log Rocket Function
+let __has_header = function () {
+    return window.LogRocket && window.LogRocket.init('f0lnrk/my-portfolio');
 };
-function onLoad() { 
+
+// Page Speed On Browser about Application Performance
+function onLoad() {
     var now = new Date().getTime();
-    var page_load_time = window.performance.timing.domContentLoadedEventEnd-window.performance.timing.navigationStart;
-    console.log("Page loaded in: " + page_load_time/1000 + ' sec');
-  }
+    var page_load_time = window.performance.timing.domContentLoadedEventEnd - window.performance.timing.navigationStart;
+    console.log("Page loaded in: " + page_load_time / 1000 + ' sec');
+}
 
+//Preloader Function
+let preloader = function (id, time){
+    $(id).fadeOut(time);
+}
